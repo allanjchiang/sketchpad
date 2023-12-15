8400602985bf3e3017ecd4f214f7a16cbce158a2
Allan Chiang <allanchiang24@gmail.com>
2023-12-15 22:05:42 +1300

    Create 256 divs

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ div1.classList.add('div1');
 
 container.appendChild(div1);
 
-for(let i = 0; i <= 16; i++) {
+for(let i = 0; i <= 256; i++) {
     const div1 = document.createElement('div');
     div1.classList.add('div1');
     container.appendChild(div1);
