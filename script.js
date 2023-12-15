4855d0db138621e23a96b4a38a8a95e7edbb280c
Allan Chiang <allanchiang24@gmail.com>
2023-12-15 21:49:20 +1300

    Create a for loop that creates div elements

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const div1 = document.createElement('div');
 div1.classList.add('div1');
 
 container.appendChild(div1);
+
+for(let i = 0; i <= 16; i++) {
+    const div1 = document.createElement('div');
+}
