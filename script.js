27b806c93b921dd9fe65d64c3a02050c0b21dac2
Allan Chiang <allanchiang24@gmail.com>
2023-12-19 13:58:07 +1300

    Remove append child for one div from code

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const container = document.querySelector('.container');
 
-const div1 = document.createElement('div');
-div1.classList.add('div1');
-
-container.appendChild(div1);
-
 for(let i = 0; i <= 256; i++) {
     const div1 = document.createElement('div');
     div1.classList.add('div1');
