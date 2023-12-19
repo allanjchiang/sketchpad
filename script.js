c80d14ee81c1f5565513cc6583e23cee68c1da92
Allan Chiang <allanchiang24@gmail.com>
2023-12-19 14:01:24 +1300

    Loop through all divs and add mouseover event listener to each div

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const container = document.querySelector('.container');
 const divs = document.querySelectorAll('div');
 
+divs.forEach((div) => {
+    div.addEventListener('mouseenter', () => {
+        
+    });
+});
+
 for(let i = 0; i <= 256; i++) {
     const div1 = document.createElement('div');
     div1.classList.add('div1');
