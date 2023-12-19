d699f6a2985a34b79d879b89d7dfedb114370e7c
Allan Chiang <allanchiang24@gmail.com>
2023-12-19 15:24:05 +1300

    Test alert in event listener

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const prompt = document.querySelector('.prompt');
 const container = document.querySelector('.container');
 
 prompt.addEventListener('click', () => {
-    
+    alert('Hello world!');
 });
 
 
