3c6988f903a93cc01e569a05d466189caa8dd880
Allan Chiang <allanchiang24@gmail.com>
2023-12-20 15:10:36 +1300

    Align .container in center

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ size.addEventListener('click', () => {
     for(let i = 0; i <= sizeTest * sizeTest; i++) {
         const div1 = document.createElement('div');
         div1.classList.add('div1');
-        div1.textContent = i;
         container.appendChild(div1);
     }
 });
