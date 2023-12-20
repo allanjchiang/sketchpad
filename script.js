2f762943ee86508e6b88aa3de01b061b2bb64753
Allan Chiang <allanchiang24@gmail.com>
2023-12-20 16:02:10 +1300

    Add document.addEventListener DOMContentLoaded with a function that gets container and button by id

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-const size = document.querySelector('#size');
-const container = document.querySelector('.container');
-
-size.addEventListener('click', () => {
-    let sizeTest = prompt("What size were you after?");
-    for(let i = 0; i <= sizeTest * sizeTest; i++) {
-        const div1 = document.createElement('div');
-        div1.classList.add('div1');
-        container.appendChild(div1);
-    }
-});
-
-
-
-
-const divs = document.querySelectorAll('.div1');
-
-divs.forEach((div) => {
-    div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = 'lightgreen';
-    });
-
-    div.addEventListener('mouseleave', () => {
-        div.style.backgroundColor = 'white';
-    });
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const gridContainer = document.getElementById('grid-container');
+    const changeSizeButton = document.getElementById('change-size');
+})
