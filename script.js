d34e9992dab3c0dc890aa3c02e9a771870e4d457
Allan Chiang <allanchiang24@gmail.com>
2023-12-20 16:02:28 +1300

    Make grid size 16

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const gridContainer = document.getElementById('grid-container');
     const changeSizeButton = document.getElementById('change-size');
+
+    let gridSize = 16;
 })
