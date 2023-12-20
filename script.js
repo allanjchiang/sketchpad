e8a5dfef969cf79865284c30220310be2eecf29e
Allan Chiang <allanchiang24@gmail.com>
2023-12-20 14:55:50 +1300

    Transfer for loop that generates divs inside event listener function

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const container = document.querySelector('.container');
 
 size.addEventListener('click', () => {
     let sizeTest = prompt("What size were you after?");
+    for(let i = 0; i <= sizeTest * sizeTest; i++) {
+        const div1 = document.createElement('div');
+        div1.classList.add('div1');
+        container.appendChild(div1);
+    }
 });
 
 
-for(let i = 0; i <= sizeTest * sizeTest; i++) {
-    const div1 = document.createElement('div');
-    div1.classList.add('div1');
-    container.appendChild(div1);
-}
+
 
 const divs = document.querySelectorAll('.div1');
 
